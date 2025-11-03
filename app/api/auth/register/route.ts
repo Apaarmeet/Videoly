fc7dbb72c05aa196c9fc7f9a1b8ee92dbe07a673
apaarmeet <apaarmeet5000@gmail.com>
2025-11-03 16:36:03 +0530

    initial setup and nextauth setup

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { error } from "console";
+import { NextResponse,NextRequest } from "next/server";
+
+export async function POST(request: NextRequest){
+    try{
+       const {email,password} =  await request.json()
+
+       if(!email || !password){
+        return NextResponse.json(
+            {error: "Email and Password are required"},
+            {status: 400}
+        )
+       }
+
+       await connectToDatabase()
+
+      const existingUser = await User.findOne({email})
+      if(existingUser){
+        return NextResponse.json(
+            {error: "User already registered"},
+            {status: 400}
+        )
+      }
+
+     await User.create({email,password})
+     return NextResponse.json(
+        {message: "User registered Successfully"},
+        {status: 200}
+        )
+
+    } catch(error){
+        console.error("Registeration error:", error)
+        return NextResponse.json(
+            {error: "failed register user"},
+            {status: 400}
+        )
+    }
+}
